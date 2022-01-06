3cafd3c3ce16b85607274784d09d39c3338f08cd
DeltaShifter <61344702+DeltaShifter@users.noreply.github.com>
2022-01-06 21:56:48 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
-# MegaPixel4CT
-Another Magisk module let Pixel4XL support China Telecom , with some other goods  
-Pixel4XL电信模块，增加一些额外的功能
+## Telecom operators
+
+Add support for unsupported operators so that Volte is already in normal use with NR
+
+## Changelog
+<br>https://github.com/ender-zhao/Pixel-5-operator-network-unlock
+<br> by Ender Zhao CLUB
+
+## Donate
+
+<br>https://github.com/ender-zhao/EZc
+
+## ---------------------------------------
+
+以上是原模块作者信息，感谢他的模块，我进行了一些修改让P4XL能得到一些自己想用的功能:
+1.为Pixel4XL增加电信和Volte支持
+2.启用了Motion Sense功能
+3.修改Captive路径为MIUI的服务器，去掉网络连接受限
+By 酷安 DaleSaika,传播请保留原模块作者信息
---
+++ install.sh
@@ -0,0 +1,45 @@
+SKIPMOUNT=false
+
+PROPFILE=true
+
+POSTFSDATA=true
+
+LATESTARTSERVICE=true
+
+  ui_print "Module installation is complete."
+  ui_print "************************************"
+  ui_print "             Pixel 4 XL "
+  ui_print "               3 in 1 "
+  ui_print "Thank you for choosing this module"
+  ui_print "************************************"
+  ui_print "- Cleaning radio cache..."
+rm -r /data/vendor/radio
+rm -r /data/vendor/modem_fdr
+rm /data/vendor/radio/iccid_0
+rm /data/vendor/radio/qcril.db
+rm /data/vendor/radio/qcril_backup.db
+  ui_print "- Execution complete!"
+
+REPLACE="
+/vendor/rfs/msm/mpss/readonly/vendor/mbn/mcfg_sw
+/system/vendor/rfs/msm/mpss/readonly/vendor/mbn/mcfg_sw
+"
+
+on_install() {
+  ui_print "- Extracting module files"
+  unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
+  }
+  ui_print "- Parameter compiling"
+  
+  set_permissions() {
+  set_perm_recursive $MODPATH/system/vendor/mbn 0 0 0755 0644 u:object_r:vendor_file:s0
+  set_perm_recursive $MODPATH 0 0 0755 0644 u:object_r:vendor_file:s0
+}
+
+  ui_print "- Fully completed"
+  ui_print "______________________________________________"
+  ui_print "           Original created by E.Z.C"
+  ui_print "   Consider donating if you like this module."
+  ui_print "               Modded by DaleSaika."
+  ui_print "                    Coolapk"
+  ui_print "----------------------------------------------"
